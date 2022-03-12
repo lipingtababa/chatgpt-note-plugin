9792a16e22b4826b1f88a627f4eb04a2a4184baa
lipingtababa <hellomarch@gmail.com>
2022-03-12 19:35:51 +0100

    Basic infor

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Ma Chi',
+    site_url: 'https://verceldemo-mocha.vercel.app/',
+    feed_url: 'https://verceldemo-mocha.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
