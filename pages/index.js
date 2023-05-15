7d6ed12ada04aedd6203390f5724049c7a6bac7b
Ma Chi <hellomarch@gmail.com>
2023-05-15 04:46:24 +0200

    update description

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default function Page() {
   // return link to todos page
-  return React.createElement("a", { href: "/api/todos" }, "My Todos");
+  return React.createElement("a", { href: "/api/todos" }, "My Todo List");
   }
